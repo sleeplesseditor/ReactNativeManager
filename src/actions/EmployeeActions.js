c5f837e1221b13f0fd7bbb621c2234c7777169c6
sleeplesseditor <josh@bryson.me.uk>
2018-05-02 23:56:03 +0100

    App finalised

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -46,7 +46,7 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
             .set({ name, phone, shift })
             .then(() => {
                 dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
-                Actions.employeeList({ type: 'reset' })
+                Actions.pop({ type: 'reset' })
             });
     };
 };
@@ -58,7 +58,7 @@ export const employeeDelete = ({ uid }) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
             .remove()
             .then(() => {
-                Actions.employeeList({ type: 'reset' });
+                Actions.pop({ type: 'reset' });
             });
     };
 };
